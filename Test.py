38c3b8ea059683fdeb8b6adaffbf1a6a7c470429
Amit Yadav <amit.accuster07@gmail.com>
2024-12-18 13:10:32 +0530

    Initial stage

---
+++ Test.py
@@ -0,0 +1,9 @@
+users = {"admin": "1234", "user1": "abcd"}
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username in users and users[username] == password:
+    print("Login successful!")
+else:
+    print("Invalid username or password.")
